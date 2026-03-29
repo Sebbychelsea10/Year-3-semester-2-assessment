48d2fbbbb984b0900b766a81b6cdc52fd9b8d375
Sebbychelsea10 <c3009072@hallam.shu.ac.uk>
2026-03-29 16:33:07 +0100

    added a settings proivder page

---
+++ lib/providers/setting_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingProvider with ChangeNotifier {
+
+  bool _darkMode = false;
+  bool _notificationsEnabled = true;
+
+  bool get darkMode => _darkMode;
+  bool get notificationsEnabled => _notificationsEnabled;
+
+  SettingProvider() {
+    _loadSettings();
+  }
+
+  void _loadSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    _darkMode = prefs.getBool("darkMode") ?? false;
+    _notificationsEnabled = prefs.getBool("notifications") ?? true;
+
+    notifyListeners();
+  }
+
+  void setDarkMode(bool value) async {
+    _darkMode = value;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool("darkMode", value);
+    notifyListeners();
+  }
+
+  void setNotifications(bool value) async {
+    _notificationsEnabled = value;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool("notifications", value);
+    notifyListeners();
+  }
+}
